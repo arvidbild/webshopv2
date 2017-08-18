3d3e8e213763d62ae58561f8ef4eaf228d2ca452
Arvid <arvid.bild@gmail.com>
2017-08-18 16:58:44 +0200

    according to the v.1

--- app.js
+++ app.js
@@ -4,15 +4,54 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var morgan = require("morgan");
+var pug = require("pug");
+var passport = require("passport");
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+//mongoose
+var mongoose = require("mongoose");
+
+//sessions
+var session = require("express-session");
+var MongoStore = require("connect-mongo")(session);
+
+//, data.json and seeder
+var seeder = require("mongoose-seeder");
+var data = require("./src/data/data.json");
+
+//reuire our routes
+var index = require('./src/routes/index');
+var users = require('./src/routes/users');
+var api = require("./src/routes/api");
+var cart = require("./src/routes/cart");
+require('./config/passport');
 
 var app = express();
 
+/** 
+Connect to the database
+**/
+mongoose.connect("mongodb://localhost:27017/webshop");
+require("./config/passport.js");
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console,"connetion error"));
+
+db.once("open", function() {
+    seeder.seed(data).then(function(){
+    console.log("the data has been seeded");
+    }).catch(function(err) {
+    // handle error
+        console.log(err);
+    }); 
+});
+
+//Set the port 
+app.set("port", process.env.PORT || 5000);
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.set('views', "./public/views");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +61,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// morgan gives us http request logging
+app.use(morgan('dev'));
+
+// Use the session middleware
+app.use(session({ 
+             secret: 'keyboard cat', 
+     //       resave: false, //don't save session if unmodified 
+  // saveUninitialized: false, // don't create session until something stored 
+              store: new MongoStore({mongooseConnection: mongoose.connection}),
+             cookie: {maxAge: 180 * 60 * 1000} 
+}));
+
+//middleware for the session to be reached too all views
+app.use(function(req,res,next){
+  res.locals.session = req.session;
+  next();
+})
+
+//Initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// setup our static route to serve files from the "public" folder
+app.use("/", express.static('public'));
+
+//Setup for our routes
 app.use('/', index);
 app.use('/users', users);
+app.use("/api", api);
+app.use("/cart", cart);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
