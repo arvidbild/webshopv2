551bba3c862f126e6174515ca1ab8b8f890cdc21
Arvid <arvid.bild@gmail.com>
2017-08-20 12:01:44 +0200

    removed favicon for now since it gives me an error when pushin to Heroku

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -53,8 +52,7 @@ app.set("port", process.env.PORT || 5000);
 app.set('view engine', 'pug');
 app.set('views', "./public/views");
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
