b914423e822df7dc0778af71c52cce9d06644191
Arvid <arvid.bild@gmail.com>
2017-08-20 12:22:43 +0200

    added app.listen which will listening to the port wich Heroku dinamicly sets

--- app.js
+++ app.js
@@ -48,6 +48,10 @@ db.once("open", function() {
 //Set the port 
 app.set("port", process.env.PORT || 5000);
 
+app.listen(process.env.PORT || 5000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
 // view engine setup
 app.set('view engine', 'pug');
 app.set('views', "./public/views");
