2023020e7fc122835630a0f6801f16a71b870821
Arvid <arvid.bild@gmail.com>
2017-08-22 12:42:31 +0200

    nothing

--- config/passport.js
+++ config/passport.js
@@ -3,8 +3,8 @@ var User = require('../src/models/User');
 var FacebookStrategy = require("passport-facebook").Strategy;
 
 //Uncomment this in local enviroment to get the clientID & clientSecret
-//var passwords = require("./passwords/passwords.js");
-//console.log(passwords);
+var passwords = require("./passwords/passwords.js");
+console.log(passwords);
 
 passport.serializeUser(function (user, done) {
     done(null, user.id);
@@ -21,9 +21,9 @@ console.log(port);
 //use facebook login with passport strategy (process.env. is enviroment variables)
 //use passwords.clientID in localenviroment and localhost:5000
 passport.use(new FacebookStrategy({
-    clientID: process.env.clientID, 
-    clientSecret: process.env.clientSecret,
-    callbackURL: "http://"+ port +"/users/auth/facebook/callback",
+    clientID: passwords.clientID, 
+    clientSecret: passwords.clientSecret,
+    callbackURL: "http://localhost:5000/users/auth/facebook/callback",
     profileFields: ['id', 'displayName', 'link', 'photos', 'emails']
   },
     function (accessToken, refreshToken, profile, done){
