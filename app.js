19eea682217250abfc92ee5734d3049cfa3ad092
Arvid <arvid.bild@gmail.com>
2017-08-22 19:10:16 +0200

    added express-validator

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var morgan = require("morgan");
 var pug = require("pug");
 var passport = require("passport");
 var flash = require("connect-flash");
+var validator = require('express-validator');
+
 
 //mongoose
 var mongoose = require("mongoose");
@@ -67,6 +69,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(validator());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // morgan gives us http request logging
